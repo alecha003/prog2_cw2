e225ec5b4a88c37eb6150dd4299c2ad7a8e30481
alecha003 <alecha003@utp.edu.pl>
2021-03-24 19:08:18 +0100

    commit1

---
+++ src/Task.java
@@ -0,0 +1,8 @@
+public class Task {
+    private String taskName;
+    private String taskDescription;
+    private String taskCategory;
+    private String taskDateTimeStart;
+    private String taskDateTimeStop;
+
+}
